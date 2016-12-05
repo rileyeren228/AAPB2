1014f88d1dbcbe7c6dfc2d595d753bf6b0f4fb8a
rfraimow <ryfkah@gmail.com>
2016-12-05 16:29:06 -0500

    Update makebags.sh

--- scripts/makebags.sh
+++ scripts/makebags.sh
@@ -12,8 +12,11 @@ read -p "please submit the volume where the bags are to be written
 r=$(echo $r1 | tr -d ' ')
 bags="$r"
 
+mkdir $bags/audio
+mkdir $bags/video
 
-for d in $(find $ingest -type d -maxdepth 1 -mindepth 1); do
+
+for d in $(find $ingest -type d -not -path '*/\.*' -name 'cpb*' -maxdepth 1 -mindepth 1); do
 	guid=$(echo $d | tr / '\n' | grep -vx '^$' | tail -1)
 	echo $guid
 	mkdir $bags/$guid 
@@ -22,7 +25,7 @@ for d in $(find $ingest -type d -maxdepth 1 -mindepth 1); do
 		echo $f
 		file=$(echo $f | tr / '\n' | grep -vx '^$' | tail -1)
 		echo $file
-		mediainfo -f --Language=Raw --Output=XML $f > $bags/$guid/master/${file}.mediainfo.xml
+		##mediainfo -f --Language=Raw --Output=XML $f > $bags/$guid/master/${file}.mediainfo.xml
 	done
 	for m in $(find $d -type f -path '*mezz*'); do
 		echo $m
@@ -34,11 +37,22 @@ for d in $(find $ingest -type d -maxdepth 1 -mindepth 1); do
 		proxy=$(echo $p | tr / '\n' | grep -vx '^$' | tail -1)
 		mediainfo -f --Language=Raw --Output=XML $p > $bags/$guid/proxy/${proxy}.mediainfo.xml
 	done
-	/Users/$USER/bagit-python/bagit.py $bags/$guid
-	cd $bags
-	zip -r ${guid}-sparse.zip $guid
-	find . -type d -maxdepth 1 -mindepth 1 -not -name "*.zip" -exec rm -R {} \;	
-
+	
+	mediainfo=$(find $bags/$guid/proxy/ -type f -name '*mediainfo*')
+	if grep 'Video' $mediainfo; then
+		mv $bags/$guid $bags/video/$guid
+		/Users/$USER/bagit-python/bagit.py $bags/video/$guid
+		cd $bags/video
+		zip -r ${guid}-sparse.zip $guid
+		find . -type d -maxdepth 1 -mindepth 1 -not -name "*.zip" -exec rm -R {} \;	
+		cd $bags
+	else
+		mv $bags/$guid $bags/audio/$guid
+		/Users/$USER/bagit-python/bagit.py $bags/audio/$guid
+		cd $bags/audio
+		zip -r ${guid}-sparse.zip $guid
+		find . -type d -maxdepth 1 -mindepth 1 -not -name "*.zip" -exec rm -R {} \;	
+	fi
 done
 
 IFS=$SAVEIFS
